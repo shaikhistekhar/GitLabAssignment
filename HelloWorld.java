3604ab7d94293cdd4a969c567191ad5a7260242e
shaikhistekhar <shaikhistekhar@gmail.com>
2025-11-28 00:26:10 +0530

    Updated java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI! second");
+        System.out.println("Hello, Jenkins CI! second, night shift");
     }
 }
