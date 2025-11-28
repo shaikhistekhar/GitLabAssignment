b5c46b18092129859e89cd4ab87f81fde232b725
shaikhistekhar <shaikhistekhar@gmail.com>
2025-11-28 20:35:38 +0530

    Updated jave file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI! second, night shift");
+        System.out.println("Hello, Jenkins CI! second, night shifttts");
     }
 }
